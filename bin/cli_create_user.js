0686a67f484d95e41bdf800ead3e0a079bc880d1
Jurgo Boemo <jurgo.boemo@gmail.com>
2021-03-30 13:21:36 +0100

    creat user util fix

--- bin/cli_create_user.js
+++ bin/cli_create_user.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const {generateBEToken,generateUserToken,getStaticConfig} = require('./utils');
+const {generateBEToken,generateUserToken,getStaticConfig} = require('../src/utils');
 
 const config = getStaticConfig(process.env)
 
